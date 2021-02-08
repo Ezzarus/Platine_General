864c643ce867017a0e61df4d9fa11c88ea801984
Lucas Derache <lucas.derache.etu@univ-lille.fr>
2021-02-08 02:27:04 +0100

    [feat]creating commentaire table

---
+++ api/src/main/java/platine/workmood/api/model/Commentaire.java
@@ -0,0 +1,46 @@
+package platine.workmood.api.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "commentaire")
+public class Commentaire {
+    private int id;
+    private String dateHeure;
+    private boolean anonyme;
+
+    public Commentaire() {
+    }
+
+    public Commentaire(int id, String dateHeure, boolean anonyme) {
+        this.id = id;
+        this.dateHeure = dateHeure;
+        this.anonyme = anonyme;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDateHeure() {
+        return dateHeure;
+    }
+
+    public void setDateHeure(String dateHeure) {
+        this.dateHeure = dateHeure;
+    }
+
+    public boolean isAnonyme() {
+        return anonyme;
+    }
+
+    public void setAnonyme(boolean anonyme) {
+        this.anonyme = anonyme;
+    }
+}
