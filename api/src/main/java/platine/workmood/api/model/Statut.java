6ce410be3338a171a2df7ecb379508fc1b4c38c2
Lucas Derache <lucas.derache.etu@univ-lille.fr>
2021-02-08 02:10:08 +0100

    [feat]creating statut table

---
+++ api/src/main/java/platine/workmood/api/model/Statut.java
@@ -0,0 +1,36 @@
+package platine.workmood.api.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "statut")
+public class Statut {
+    private int id;
+    private String intitule;
+
+    public Statut() {
+    }
+
+    public Statut(int id, String intitule) {
+        this.id = id;
+        this.intitule = intitule;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getIntitule() {
+        return intitule;
+    }
+
+    public void setIntitule(String intitule) {
+        this.intitule = intitule;
+    }
+}
